47e3eecba1098cc01dad4ba4ecf3c018aef70f7e
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2023-05-04 16:19:56 +0100

    adding route for finding farmer/application by term instead of all of them

--- routes/application.js
+++ routes/application.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getApplication, postApplication } from "../controllers/application.js";
+import { getApplication, getApplicationByID, postApplication } from "../controllers/application.js";
 
 const router = Router();
 
 router.get("/", getApplication);
+router.get("/:id", getApplicationByID);
 router.post("/", postApplication);
 
 export default router;
--- routes/farmers.js
+++ routes/farmers.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getFarmers } from "../controllers/farmers.js";
+import { getFarmers, postFarmers, getFarmerByName } from "../controllers/farmers.js";
 
 const router = Router();
 
 router.get("/", getFarmers);
+router.post("/", postFarmers);
+router.get("/:name", getFarmerByName);
 
 export default router;
