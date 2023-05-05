f47da7b87a55474efe89be70823b51b5b58fc57d
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2023-05-05 14:58:04 +0100

    added error handling and status codes to farm.js

--- controllers/farm.js
+++ controllers/farm.js
@@ -1,19 +1,33 @@
 import fs from 'fs';
-const data = JSON.parse(
-  fs.readFileSync('controllers/data.json')
-);
+
+try {
+  const data = JSON.parse(fs.readFileSync('controllers/data.json'));
+} catch (err) {
+  console.error(err);
+  res.status(500).send('Internal Server Error');
+}
 
 export function getFarm(req, res, next) {
-  return res.json(data.farm)
-}
+  try {
+    return res.status(200).json(data.farm)
+  } catch (error) {
+    console.error("Error while processing request:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  };
 
-export function postFarm(req, res, next) {
-  data['farm'].push(req.body);
-  fs.writeFile('controllers/data.json', JSON.stringify(data, null, 2), err => {
-    if (err) {
-      console.log("error writing file:", err)
-      return
-    };
-  });
-  return res.json(data)
-}
+  export function postFarm(req, res, next) {
+    try {
+      data['farm'].push(req.body);
+      fs.writeFile('controllers/data.json', JSON.stringify(data, null, 2), err => {
+        if (err) {
+          console.error("Error writing file:", err)
+          return res.status(500).json({ error: "Internal Server Error" })
+        };
+        return res.status(201).json(data)
+      });
+    } catch (error) {
+      console.error("Error while processing request:", error)
+      return res.status(400).json({ error: "Bad Request" })
+    }
+  }
+};
