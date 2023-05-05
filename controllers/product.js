b556d00c2463ed262f390908d741b033b229a585
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2023-05-05 15:14:45 +0100

    addeed error handling and status codes to product.js

--- controllers/product.js
+++ controllers/product.js
@@ -4,16 +4,29 @@ const data = JSON.parse(
 );
 
 export function getProduct(req, res, next) {
-  return res.json(data.product)
+  try {
+  if (!data.product || data.product.length === 0) {
+      return res.status(404).json({ error: "No Products Found" });
+  }
+    return res.status(200).json(data.product);
+  } catch (error) {
+    console.log("error:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 }
 
 export function postProduct(req, res, next) {
+  try {
   data['product'].push(req.body);
   fs.writeFile('controllers/data.json', JSON.stringify(data, null, 2), err => {
     if (err) {
-      console.log("error writing file:", err)
-      return
+      console.log("Error Writing File:", err)
+      return res.status(500).json({ error: "Internal Server Error" });
     };
   });
-  return res.json(data)
+  return res.status(201).json(data);
+} catch (error) {
+    console.error(error);
+    return res.status(400).json({ error: "Bad Request" });
+  }
 }
