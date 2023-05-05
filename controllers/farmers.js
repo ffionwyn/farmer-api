72458abd9b1f3953c38923aa0b7a8493207b338b
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2023-05-05 15:10:35 +0100

    added error handling and status codes to farmers.js

--- controllers/farmers.js
+++ controllers/farmers.js
@@ -4,35 +4,53 @@ const data = JSON.parse(
 );
 
 export function postFarmers(req, res, next) {
-  data['farmer'].push(req.body);
-  fs.writeFile('controllers/data.json', JSON.stringify(data, null, 2), err => {
-    if (err) {
-      console.log("error writing file:", err)
-      return
-    };
-  });
-  return res.json(data)
+  try {
+    data['farmer'].push(req.body);
+    fs.writeFile('controllers/data.json', JSON.stringify(data, null, 2), err => {
+      if (err) {
+        console.log("error writing file:", err);
+        return res.status(500).json({ error: "Internal Server Error" });
+      };
+    });
+    return res.status(201).json(data);
+  } catch (error) {
+    console.log("error:", error);
+    return res.status(400).json({ error: "Bad Request" });
+  }
 }
 
 export function getFarmers(req, res, next) {
-  const limit = req.query.limit != null ? req.query.limit : 30;
-  const start = req.query.start != null ? req.query.start : 0;
-  const farmers = data.farmer.slice(start, limit)
+  try {
+    const limit = req.query.limit != null ? req.query.limit : 30;
+    const start = req.query.start != null ? req.query.start : 0;
+    const farmers = data.farmer.slice(start, limit)
 
-  const farmersLeft = data.farmer.length - start 
-  const seeMore = { "seeMore": farmersLeft > limit }
+    const farmersLeft = data.farmer.length - start
+    const seeMore = { "seeMore": farmersLeft > limit };
 
-  farmers.push(seeMore)
-  return res.json(farmers)
+    farmers.push(seeMore)
+    return res.status(200).json(farmers);
+  } catch (error) {
+    console.log("error:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 }
 
 export function getAllFarmerInfo(req, res, next) {
+  try {
   const id = req.params.id;
   const farmer = data.farmer.find(farmer => { return farmer.id == id });
-  const farm = data.farm.find(farm => { return farm.id == farmer.farm_id });
-
+    if (!farmer) {
+      return res.status(404).json({ error: "Farmer Not Found" });
+    }
+    const farm = data.farm.find(farm => { return farm.id == farmer.farm_id });
+    if (!farm) {
+      return res.status(404).json({ error: "Farm Not Found" });
+    }
   const farmerAndFarmTogether = [farmer,farm];
-  return res.json(farmerAndFarmTogether)
+  return res.status(200).json(farmerAndFarmTogether);
+} catch (error) {
+    console.log("error:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 }
-
-
